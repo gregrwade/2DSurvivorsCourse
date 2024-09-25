23006f451e634f0b81340bdd583b247e8715f0b7
Greg Wade <greg.r.wade@gmail.com>
2024-09-25 17:36:00 -0400

    test

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+const MAX_SPEED = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+	
+
+func get_movement_vector():
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	return Vector2(x_movement, y_movement)
